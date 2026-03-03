3cf32345df6961613216e374f9fb099d8aa978f2
zbig2016zbig-code <zbig2016zbig@gmail.com>
2026-03-03 16:48:21 +0100

    Update entrypoint.sh

--- afore_shinebus_app/entrypoint.sh
+++ afore_shinebus_app/entrypoint.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
+
+python3 /app/shinebus.py \
+  --mqtt-host "$MQTT_HOST" \
+  --mqtt-port "$MQTT_PORT" \
+  --mqtt-username "$MQTT_USERNAME" \
+  --mqtt-password "$MQTT_PASSWORD" \
+  --mqtt-topic "$MQTT_TOPIC" \
+  --inverter-host "$INVERTER_HOST" \
+  --inverter-port "$INVERTER_PORT" \
+  --interval "$INTERVAL"
 
